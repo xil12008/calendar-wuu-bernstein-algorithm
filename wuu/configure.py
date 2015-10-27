d5eaaae00b3bb729243917ed13b0d0ad95f870ea
EC2 Default User <ec2-user@ip-172-31-0-64.ec2.internal>
2015-10-27 14:33:03 +0000

    IP changed

--- wuu/configure.py
+++ wuu/configure.py
@@ -6,7 +6,7 @@ from urllib2 import urlopen
 '''
 
 class Configuration:
-    IPTABLE = ['52.89.158.138','52.88.245.160','54.175.50.224','54.165.49.59']
+    IPTABLE = ['52.91.98.176','52.88.245.160','54.175.50.224','54.165.49.59']
     PORT = 12345 #All nodes will use this port
   
     @staticmethod
